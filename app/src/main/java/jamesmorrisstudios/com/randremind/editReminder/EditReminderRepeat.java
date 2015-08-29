ac46c7f90550f5144a6c7526d71ef979ee1c3af2
James Morris <morris.a.james@gmail.com>
2015-08-28 20:01:15 -0700

    Updated the day of week picker

--- app/src/main/java/jamesmorrisstudios/com/randremind/editReminder/EditReminderRepeat.java
+++ app/src/main/java/jamesmorrisstudios/com/randremind/editReminder/EditReminderRepeat.java
@@ -2,7 +2,9 @@ package jamesmorrisstudios.com.randremind.editReminder;
 
 import android.content.DialogInterface;
 import android.support.annotation.NonNull;
+import android.util.TypedValue;
 import android.view.View;
+import android.webkit.WebSettings;
 import android.widget.TextView;
 
 import com.jamesmorrisstudios.appbaselibrary.dialogHelper.MultiChoiceRequest;
@@ -19,7 +21,7 @@ import jamesmorrisstudios.com.randremind.reminder.ReminderList;
  * Created by James on 6/10/2015.
  */
 public class EditReminderRepeat {
-    private View weeksContainer;
+    private View weeksContainer, daysContainer;
     private TextView weeks;
     private ButtonCircleFlat[] dayButtons = new ButtonCircleFlat[7];
 
@@ -33,6 +35,7 @@ public class EditReminderRepeat {
         dayButtons[6] = (ButtonCircleFlat) parent.findViewById(R.id.day6);
         initDaysOfWeek();
         weeksContainer = parent.findViewById(R.id.weeksContainer);
+        daysContainer = parent.findViewById(R.id.daysContainer);
         weeks = (TextView) parent.findViewById(R.id.weeks);
     }
 
@@ -47,6 +50,7 @@ public class EditReminderRepeat {
         repeatDaysListener();
         weeksListener();
         updateWeeksToRun();
+        updateDaysOfWeek();
     }
 
     private void weeksListener() {
@@ -131,6 +135,7 @@ public class EditReminderRepeat {
             TextView text = dayButtons[i].getTextView();
             if (text != null) {
                 text.setText(week[i]);
+                text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 12);
             }
         }
     }
@@ -159,6 +164,42 @@ public class EditReminderRepeat {
      * Add repeat category listeners
      */
     private void repeatDaysListener() {
+        daysContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ReminderItem remind = ReminderList.getInstance().getCurrentReminder();
+                if (remind == null) {
+                    return;
+                }
+                String title = AppUtil.getContext().getString(R.string.days_of_week);
+                String[] week = UtilsTime.getWeekStringArray();
+                final boolean[] checked = remind.getDaysToRun();
+                Bus.postObject(new MultiChoiceRequest(title, week, checked, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                        checked[which] = isChecked;
+                    }
+                }, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ReminderItem remind = ReminderList.getInstance().getCurrentReminder();
+                        if (remind == null) {
+                            return;
+                        }
+                        remind.setDaysToRun(checked);
+                        updateDaysOfWeek();
+                    }
+                }, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //On Negative. Unused
+                    }
+                }));
+            }
+        });
+
+
+
         //Day of week selector
         for (int i = 0; i < dayButtons.length; i++) {
             final int index = i;
@@ -175,4 +216,15 @@ public class EditReminderRepeat {
             });
         }
     }
+
+    private void updateDaysOfWeek() {
+        ReminderItem remind = ReminderList.getInstance().getCurrentReminder();
+        if (remind == null) {
+            return;
+        }
+        for (int i = 0; i < dayButtons.length; i++) {
+            setDayOfWeek(i, remind.getDaysToRun()[i]);
+        }
+    }
+
 }
